c94c71de9b1ab454aa54affb1b19a43941f2defb
1-21Gigawatts <1.21gigawatts88mph@gmail.com>
2015-06-22 11:17:05 -0500

    JQuery testing in Chipsets.

---
+++ motherboard/chipsets/chipsets.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    
+    $(".info_hidden").hide();
+    
+    $('.info_box').click(function() {
+        $(".info_hidden").toggle();  
+        
+    });
+        
+    
+});
