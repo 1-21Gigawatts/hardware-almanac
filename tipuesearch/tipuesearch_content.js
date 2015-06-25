b652d460063c50a226de26ae53b607a60910ac8c
1-21Gigawatts <1.21gigawatts88mph@gmail.com>
2015-06-25 11:24:44 -0500

    added search functionality

---
+++ tipuesearch/tipuesearch_content.js
@@ -0,0 +1,17 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Memory", "text": "", "tags": "memory RAM stick DDR DDR2 DDR3 DDR4 DIMM SO-DIMM FB-DIMM", "url": "../memory/memory.html"},
+        {"title": "DDR", "text": "", "tags": "memory RAM stick DDR DIMM SO-DIMM", "url": "../memory/ddr/ddr.html"},
+        {"title": "DDR2", "text": "", "tags": "memory RAM stick DDR2 DIMM SO-DIMM FB-DIMM", "url": "../memory/ddr2/ddr2.html"},
+        {"title": "DDR3", "text": "", "tags": "memory RAM stick DDR3 DIMM SO-DIMM", "url": "../memory/ddr3/ddr3.html"},
+        {"title": "DDR4", "text": "", "tags": "memory RAM stick DDR4 DIMM SO-DIMM", "url": "../memory/ddr4/ddr4.html"},
+     {"title": "Motherboard", "text": "", "tags": "motherboard mobo mainboard", "url": "../motherboard/motherboard.html"},
+        {"title": "Sockets", "text": "", "tags": "socket CPU processor motherboard LGA PGA slot", "url": "../motherboard/cpu/sockets/sockets.html"},
+            {"title": "Intel Sockets", "text": "", "tags": "Intel socket CPU processor motherboard LGA PGA slot", "url": "../motherboard/cpu/sockets/intel/intel.html"},
+            {"title": "AMD Sockets", "text": "", "tags": "AMD socket CPU processor motherboard PGA slot", "url": "../motherboard/cpu/sockets/amd/amd.html"},
+        {"title": "Chipsets", "text": "", "tags": "chipset Intel AMD motherboard northbridge southbridge Point Peak burg", "url": "../motherboard/chipsets/chipsets.html"},
+            {"title": "Intel Chipsets", "text": "", "tags": "Intel chipset motherboard northbridge southbridge Point Peak burg", "url": "../motherboard/chipsets/intel/intel.html"}
+]};
+
+
+
